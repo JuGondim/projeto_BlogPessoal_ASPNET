669079cf7834841f790380870ca4ecdb35241671
Juliana <juh.ngn@gmail.com>
2022-08-09 21:50:32 -0300

    @JuGondim [Task: Criando camada de usuário Part. 1] {09/08/2022}

---
+++ BlogAPI/Src/Repositorios/Implementacoes/UsuarioRepositorio.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogAPI.Src.Contextos;
+using BlogAPI.Src.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogAPI.Src.Repositorios.Implementacoes
+{
+    public class UsuarioRepositorio : IUsuario
+    {
+        #region Atributos
+        private readonly BlogPessoalContexto _contexto;
+        #endregion
+        #region Construtores
+        public UsuarioRepositorio(BlogPessoalContexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public Task NovoUsuarioAsync(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Usuario> PegarUsuarioPeloEmailAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #region Métodos
+        #endregion
+    }
+}
+
--- BlogAPI/Startup.cs
+++ BlogAPI/Startup.cs
@@ -1,6 +1,8 @@
+using BlogAPI.Src.Contextos;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -9,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace BlogAPI
 {
@@ -24,17 +27,23 @@ namespace BlogAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BlogPessoalContexto>(opt =>
+            opt.UseSqlServer(Configuration["ConnectionStringsDev:DefaultConnection"]));
+
 
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, BlogPessoalContexto contexto)
         {
             if (env.IsDevelopment())
             {
+                contexto.Database.EnsureCreated();
                 app.UseDeveloperExceptionPage();
             }
+
+            contexto.Database.EnsureCreated();
 
             app.UseRouting();
 
